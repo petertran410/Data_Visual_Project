02c764c7d7aabb38ec818c1a9bfff4c752b19772
bin <91893958+ITITIU20337@users.noreply.github.com>
2023-12-04 10:49:59 +0700

    update

--- src/Age/draw.js
+++ src/Age/draw.js
@@ -3,28 +3,26 @@ import { GoogleCharts } from 'google-charts';
 import * as d3 from 'd3';
 
 const Draw = () => {
-  useEffect(() => {
-    // Load data using d3.csv
-    d3.csv(
-      'https://raw.githubusercontent.com/petertran410/data_visual_project/GDP/TocDoTangTruongGRDP.csv',
-      rowConverter
-    )
-      .then((originalData) => {
-        console.log(originalData);
-        const formattedData = formatData(originalData);
-        GoogleCharts.load(() => drawChart(formattedData), { packages: ['corechart'] });
-      })
-      .catch((error) => {
-        console.error('Error loading CSV:', error);
-      });
-  }, []);
-  const rowConverter = (row) => {
-    // Customize this function to convert and format each row as needed
-    return row;
-  };
-  const formatData = (csvData) => {
-    return csvData;
-  };
+    const rowConverter = (row) => {
+    };
+
+    const formatData = (originalData) => {
+    };
+
+    useEffect(() => {
+        d3.csv(
+            '/TuoiThoBinhQuan.csv',
+            rowConverter
+        )
+            .then((originalData) => {
+                console.log(originalData);
+                const formattedData = formatData(originalData);
+                GoogleCharts.load(() => drawChart(formattedData), { packages: ['corechart'] });
+            })
+            .catch((error) => {
+                console.error('Error loading CSV:', error);
+            });
+    }, []);
   const drawChart = (data) => {
     const dataTable = GoogleCharts.api.visualization.arrayToDataTable(data);
 
@@ -52,5 +50,6 @@ const Draw = () => {
   };
 
   return <div id="chart" />;
+
 };
 export default Draw;

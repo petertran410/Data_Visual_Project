06edc0d4900f828ecd9855d3cf0b424469638f48
ThienPhuc1802 <91868450+thienphuc1802@users.noreply.github.com>
2023-12-05 23:48:05 +0700

    Update graph

--- src/GDP/GDP.js
+++ src/GDP/GDP.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Grap from './Grap';
 import * as d3 from "d3";
+import Density from '../Density/Density';
+import Graph from './Grap';
 
 const GDP = () => {
   return (
-    <div className='density' id="Density">
-      <Grap />
+    <div className='GDP' id="GDP">
+      <Graph />
     </div>
   )
 }
--- src/GDP/Grap.js
+++ src/GDP/Grap.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import * as d3 from "d3";
-
-const Grap = () => {
+const Graph = () => {
     function rowConverter(d) {
         return {
             year: d["Year"],
@@ -9,167 +8,128 @@ const Grap = () => {
 
         };
     }
-
     d3.csv(
         "https://raw.githubusercontent.com/petertran410/data_visual_project/GDP/TocDoTangTruongGRDP.csv",
         rowConverter
     )
         .then(originalData => {
-            // Filter out non-zero rows
-            // const nonZeroData = originalData.filter(row => row.province == "Toàn Quốc")
-            // let first20Rows = originalData.slice(0, 1);
-            // console.log(first20Rows);
+
             console.log(originalData);
+            const svg = d3.select("#grap")
+                .append("svg")
+                .attr("width", 500)
+                .attr("height", 500)
+                .attr("padding", 500);
+            var Tooltip = d3.select("#grap")
+                .append("div")
+                .style("opacity", 0)
+                .attr("class", "tooltip")
+                .style("background-color", "white")
+                .style("border", "solid")
+                .style("border-width", "2px")
+                .style("border-radius", "5px")
+                .style("padding", "5px")
+                .style("position", "absolute");
+            var mouseover = function (event, d) {
+                Tooltip
+                    .style("opacity", 1)
+                d3.select(this)
+                    .style("stroke", "black")
+                    .attr("opacity", d => (d.cases / 100000) + 0.7)
+            }
+            var mousemove = function (event, d) {
+                Tooltip
+                    .style("top", (event.pageY + 10) + "px")
+                    .style("left", (event.pageX + 10) + "px")
+                    .html("<br> Year: " + d.year + "<br>" +
+                        "Percentage: " + d.year1 + "<br>" +
+                        "<br>")
+            }
+            var mouseleave = function (event, d) {
+                Tooltip
+                    .style("opacity", 0)
+                    .style("top", (event.pageY + 200) + "px")
+                    .style("left", (event.pageX + 200) + "px")
+                d3.select(this)
+                    .attr("opacity", d => (d.cases / 100000) + 0.07)
+                    .style("stroke", "none")
+            }
 
 
-            your_draw_chart_function(originalData);
+            let xScale = d3.scaleBand()
+                .range([0, svg.attr("width")])
+                .domain(originalData.map(d => d.year))
+                .padding(0.4);
+
+            let yScale = d3.scaleLinear()
+                .range([500, 0])
+                .domain([0, d3.max(originalData, d => d.year1) + 2]);
+
+            let bar = svg.selectAll("rect")
+                .data(originalData)
+                .enter()
+                .append("rect")
+                .attr("x", d => xScale(d.year))
+                .attr("y", d => yScale(d.year1) - 30)
+                .attr("width", xScale.bandwidth())
+                .attr("height", d => svg.attr("height") - yScale(d.year1) + 0)
+                .attr("fill", "steelblue")
+                .on("mouseover", mouseover)
+                .on("mousemove", mousemove)
+                .on("mouseleave", mouseleave);
+
+            let labels = svg.selectAll("text")
+                .data(originalData)
+                .enter()
+                .append("text")
+                .text(d => d.year1)
+                .attr("x", d => xScale(d.year) + xScale.bandwidth() / 2)
+                .attr("y", d => yScale(d.year1) - 10)
+                .attr("text-anchor", "middle")
+                .attr("font-size", "17px")
+                .attr("fill", "black");
+
+            var yAxis = d3.axisLeft(yScale);
+            var xAxis = d3.axisBottom(xScale);
+            svg.append("g")
+                .attr("transform", "translate(" + 20 + ",-30)")
+                .call(yAxis)
+                .attr("font-size", "20px");
 
 
+            svg
+                .append("g")
+                .attr("class", "x-axis")
+                .attr("transform", "translate(0," + (svg.attr("height") - 30) + ")")
+                .call(xAxis)
+                .selectAll("text")
+                .attr("transform", "rotate(0)")
+                .attr("x", 100 / 2)
+                .attr("font-size", "15px")
+                .style("text-anchor", "end");
+
+            var width = 500;
+            var height = 500;
+            svg.append("text")
+                .attr("text-anchor", "middle")
+                .attr("x", width / 2)
+                .attr("y", height)
+                .text("Years")
+                .attr("font-family", "times new roman")
+                .attr("font-size", "20px");
 
         })
-        .catch((error) => {
-            console.log(error);
-        });
 
-    // FUNCTION HERE
-    function your_draw_chart_function(data) {
-        console.log(data);
-        // d3.select("svg").remove();
-
-        const svg = d3.select("#grap")
-            .append("svg")
-            .attr("width", 1000)
-            .attr("height", 1000)
-            .attr("padding", 500)
-
-
-        var Tooltip = d3.select("#grap")
-            .append("div")
-            .style("opacity", 0)
-            .attr("class", "tooltip")
-            .style("background-color", "white")
-            .style("border", "solid")
-            .style("border-width", "2px")
-            .style("border-radius", "5px")
-            .style("padding", "5px")
-            .style("position", "absolute")
-
-        // Three functions that change the tooltip when the user hovers / moves / leaves a bar
-        var mouseover = function (event, d) {
-            Tooltip
-                .style("opacity", 1)
-            d3.select(this)
-                .style("stroke", "black")
-                .attr("opacity", d => (d.cases / 100000) + 0.7)
-        }
-        var mousemove = function (event, d) {
-            Tooltip
-                .style("top", (event.pageY + 10) + "px")
-                .style("left", (event.pageX + 10) + "px")
-                .html("<br> Year: " + d.year + "<br>" +
-                    "Percentage: " + d.year1 + "<br>" +
-                    "<br>")
-        }
-        var mouseleave = function (event, d) {
-            Tooltip
-                .style("opacity", 0)
-                .style("top", (event.pageY + 200) + "px")
-                .style("left", (event.pageX + 200) + "px")
-            d3.select(this)
-                .attr("opacity", d => (d.cases / 100000) + 0.07)
-                .style("stroke", "none")
-        }
-
-
-        // Define x and y scales
-        var xScale = d3
-            .scaleBand()
-            .domain(data.map(d => (d.year)))
-            .range([0, svg.attr("width")])
-            .padding(0.1);
-        var yScale = d3
-            .scaleLinear()
-            .domain([0, d3.max(data, d => d.year1) + 2])
-            .range([svg.attr("height") - 30, 0]);
-
-        var xAxis = d3.axisBottom(xScale);
-        var yAxis = d3.axisLeft(yScale);
-
-        // Create x and y axes
-        svg
-            .append("g")
-            .attr("class", "x-axis")
-            .attr("transform", "translate(0," + (svg.attr("height") - 30) + ")")
-            .call(xAxis)
-            .selectAll("text")
-            .attr("transform", "rotate(0)")
-            .attr("x", 100 / 2)
-            .attr("font-size", "20px")
-            .style("text-anchor", "end");
-
-        // label
-        svg.selectAll("text")
-            .data(data)
-            .enter().append("text")
-            .text(function (d) { return d.year1 })
-            .attr("class", "text")
-            .attr("x", function (d, i) { return (i * 60) + 36 })
-            .attr("y", function (d, i) { return 415 - (d * 10) })
-            .attr("text-anchor", "middle")
-            .attr("font-family", "times new roman")
-            .attr("font-size", "20px")
-            .attr("fill", "red")
-            .style("opacity", 1);
-
-        // Create Bars
-        svg.selectAll("rect")
-            .data(data)
-            .enter()
-            .append("rect")
-            .attr("fill", "blue")
-            .attr("x", d => xScale(d.year))
-            .attr("y", d => yScale(d.year1))
-            .attr("width", xScale.bandwidth())
-            .attr("height", d => svg.attr("height") - yScale(d.year1) - 30)
-            // .attr("opacity", d => (d.year1 / 1000) + 0.07)
-            .on("mouseover", mouseover)
-            .on("mousemove", mousemove)
-            .on("mouseleave", mouseleave);
-        svg.selectAll("text")
-            .data(data)
-            .enter()
-            .append("text")
-            .text(function (d) { return d.year1 })
-            .attr("class", "text")
-            .attr("x", function (d, i) { return (i * 60) + 36 })
-            .attr("y", function (d, i) { return 415 - (d * 10) })
-            .attr("text-anchor", "middle")
-            .attr("font-family", "times new roman")
-            .attr("font-size", "20px")
-            .attr("fill", "red")
-            .style("opacity", 1);
-
-        svg.append("g")
-            .attr("transform", "translate(" + 20 + ",0)")
-            .call(yAxis)
-            .attr("font-size", "20px");
-
-    }
 
     return (
-        <div className='grap' id="grap">
-            <svg></svg>
-            {/* <button id="AddData">Add new data</button>
-            <button id="RemoveData">Remove data</button>
-            <button id="Sort">Sort</button>
-            <select id="sort">
-                <option selected value="type">Type</option>
-                <option value="name">Name</option>
-                <option value="grdp">GRDP-VND</option>
-            </select> */}
-            <svg></svg>
+        <div className='grap'>,
+
+            <div className="title" id="grap" style={{ padding: 50 }} >GRDP-Percentage               </div>
+
+
         </div>
     )
 }
 
-export default Grap;
+export default Graph;
+

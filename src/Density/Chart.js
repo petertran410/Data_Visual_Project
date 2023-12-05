b6487c14f7d9f7e078313caeb2f85839c4a1e4a7
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2023-12-05 23:34:17 +0700

    .

--- src/Density/Chart.js
+++ src/Density/Chart.js
@@ -1,66 +1,325 @@
 // import React from "react";
 // import * as d3 from "d3";
-// import { useEffect } from "react";
-// import { useState } from "react";
 
 // export default function Chart() {
-//   const [drawChart, setDrawChart] = useState({});
-//   useEffect(() => {
+//   let draw = (data) => {
+//     let width = 1500;
+//     let height = 800;
+//     let spacing = 100;
+
+//     let info = d3
+//       .select("#info")
+//       .style("position", "absolute") // Positioning
+//       .style("background", "white")
+//       .style("padding", "10px") // Modify the padding
+//       .style("border", "2px solid black") // Modify the border
+//       .style("display", "none");
+
+//     let svg = d3
+//       .select("body")
+//       .append("svg")
+//       .attr("width", width)
+//       .attr("height", height)
+//       .append("g")
+//       .attr("transform", `translate(${spacing / 2}, ${spacing / 2})`);
+
 //     d3.csv(
 //       "https://raw.githubusercontent.com/petertran410/data_visual_project/tranngocnhan/src/Density/totalpopulation.csv"
-//     )
-//       .then((result) => {
-//         console.log(result);
-//         setDrawChart(result);
-//         if (Array.isArray(result)) {
-//           let year = "2016";
-//           let dataByYear = result.map(function (d) {
-//             return {
-//               ID: d["ID"],
-//               CONVINCE: d["CONVINCE"],
-//               year: year,
-//               confirm: parseInt(d[year]),
-//             };
-//           });
-//         }
-
-//       })
-//       .catch((err) => {
-//         console.log(err);
+//     ).then(function (data) {
+//       // let date = "3/20/20";
+//       let dataByDate = data.map(function (d) {
+//         return {
+//           CONVINCE: parseFloat(d["CONVINCE"]),
+//           ID: parseFloat(d["ID"]),
+//           date: parseInt(d["2016"]),
+//           // confirmed: parseInt(d[date]),
+//         };
 //       });
-//   }, []);
-//   return <div></div>;
+//       console.log(dataByDate);
+//     });
+//     let minCONVINCE = d3.min(data, function (d) {
+//       return d.CONVINCE;
+//     });
+//     let maxCONVINCE = d3.max(data, function (d) {
+//       return d.CONVINCE;
+//     });
+//     let minID = d3.min(data, function (d) {
+//       return d.ID;
+//     });
+//     let maxID = d3.max(data, function (d) {
+//       return d.ID;
+//     });
+
+//     minCONVINCE = Math.ceil(minCONVINCE);
+//     maxCONVINCE = Math.ceil(maxCONVINCE);
+//     minID = Math.ceil(minID);
+//     maxID = Math.ceil(maxID);
+
+//     console.log(minCONVINCE, maxCONVINCE, minID, maxID);
+
+//     let xScale = d3
+//       .scaleLinear()
+//       .domain([minID, maxID])
+//       .range([0, width - spacing]);
+//     let yScale = d3
+//       .scaleLinear()
+//       .domain([minCONVINCE, maxCONVINCE])
+//       .range([height - spacing, 0]);
+
+//     var xAxis = d3.axisBottom(xScale);
+//     var yAxis = d3.axisLeft(yScale);
+
+//     // 1
+//     svg
+//       .append("g")
+//       .attr("class", "grid")
+//       .attr("transform", `translate(0, ${height - spacing / 2})`)
+//       .call(
+//         d3
+//           .axisBottom(xScale)
+//           .tickSize(-height + spacing / 2)
+//           .tickFormat("")
+//           .tickSizeOuter(0)
+//           .tickSizeInner(-height + spacing / 2)
+//           .tickPadding(10)
+//           .tickFormat("")
+//       );
+
+//     // 2
+//     svg
+//       .append("g")
+//       .attr("class", "grid")
+//       .attr("transform", `translate(${spacing / 2}, 0)`)
+//       .call(
+//         d3
+//           .axisLeft(yScale)
+//           .tickSize(-width + spacing / 2)
+//           .tickFormat("")
+//           .tickSizeOuter(0)
+//           .tickSizeInner(-width + spacing / 2)
+//           .tickPadding(10)
+//           .tickFormat("")
+//       );
+
+//     svg
+//       .append("g")
+//       .attr("id", "xAxis")
+//       .attr("transform", `translate(0, ${(height - spacing) / 2})`)
+//       .call(xAxis)
+//       .selectAll("text")
+//       .style("font-size", "14px")
+//       .style("font-weight", "bold")
+//       .filter(function (d) {
+//         return d === 0;
+//       })
+//       .attr("transform", "translate(-10,0)")
+//       .style("text-anchor", "end");
+
+//     svg
+//       .append("g")
+//       .attr("id", "yAxis")
+//       .attr("transform", `translate(${(width - spacing) / 2}, 0)`)
+//       .call(yAxis)
+//       .selectAll("text")
+//       .style("font-size", "14px")
+//       .style("font-weight", "bold")
+//       .attr("transform", "translate(0,-3)")
+//       .filter(function (d) {
+//         return d === 0;
+//       })
+//       .remove();
+
+//     svg
+//       .selectAll("circle")
+//       .data(data)
+//       .enter()
+//       .append("circle")
+//       .attr("cx", function (d) {
+//         return xScale(d.long);
+//       })
+//       .attr("cy", function (d) {
+//         return yScale(d.lat);
+//       })
+//       .attr("r", 10) // Circle size
+//       .style("fill", "blue") // Color of circles
+//       .style("opacity", 0.7) // Opacity
+//       .on("mouseover", function (d) {
+//         info.style("display", "block");
+//         info
+//           .html(
+//             `Country: ${d.country}<br>Confirmed Cases: ${d.confirmed}<br>Latitude: ${d.lat}<br>Longitude: ${d.long}`
+//           )
+//           .style("left", d3.event.pageX + 10 + "px")
+//           .style("top", d3.event.pageY - 28 + "px");
+//       })
+//       .on("mouseout", function () {
+//         info.style("display", "none");
+//       });
+//   };
+
+//   return <div>{draw()}</div>;
 // }
 
 import React, { Component } from "react";
 import * as d3 from "d3";
 
-class Chart extends Component {
-  componentDidMount() {
-    this.drawChart();
-  }
-  drawChart() {
-    const data = [12, 5, 6, 6, 9, 10];
+export default class Chart extends Component {
+  draw = (data) => {
+    let width = 1500;
+    let height = 800;
+    let spacing = 100;
 
-    const svg = d3
+    let info = d3
+      .select("#info")
+      .style("position", "absolute") // Positioning
+      .style("background", "white")
+      .style("padding", "10px") // Modify the padding
+      .style("border", "2px solid black") // Modify the border
+      .style("display", "none");
+
+    let svg = d3
       .select("body")
       .append("svg")
-      .attr("width", 700)
-      .attr("height", 300);
+      .attr("width", width)
+      .attr("height", height)
+      .append("g")
+      .attr("transform", `translate(${spacing / 2}, ${spacing / 2})`);
+
+    d3.csv(
+      "https://raw.githubusercontent.com/petertran410/data_visual_project/tranngocnhan/src/Density/totalpopulation.csv"
+    ).then(function (data) {
+      // let date = "3/20/20";
+      let dataByDate = data.map(function (d) {
+        return {
+          CONVINCE: parseFloat(d["CONVINCE"]),
+          ID: parseFloat(d["ID"]),
+          date: parseInt(d["2016"]),
+          // confirmed: parseInt(d[date]),
+        };
+      });
+      console.log(dataByDate);
+    });
+    let minCONVINCE = d3.min(data, function (d) {
+      return d.CONVINCE;
+    });
+    let maxCONVINCE = d3.max(data, function (d) {
+      return d.CONVINCE;
+    });
+    let minID = d3.min(data, function (d) {
+      return d.ID;
+    });
+    let maxID = d3.max(data, function (d) {
+      return d.ID;
+    });
+
+    minCONVINCE = Math.ceil(minCONVINCE);
+    maxCONVINCE = Math.ceil(maxCONVINCE);
+    minID = Math.ceil(minID);
+    maxID = Math.ceil(maxID);
+
+    console.log(minCONVINCE, maxCONVINCE, minID, maxID);
+
+    let xScale = d3
+      .scaleLinear()
+      .domain([minID, maxID])
+      .range([0, width - spacing]);
+    let yScale = d3
+      .scaleLinear()
+      .domain([minCONVINCE, maxCONVINCE])
+      .range([height - spacing, 0]);
+
+    var xAxis = d3.axisBottom(xScale);
+    var yAxis = d3.axisLeft(yScale);
+
+    // 1
+    svg
+      .append("g")
+      .attr("class", "grid")
+      .attr("transform", `translate(0, ${height - spacing / 2})`)
+      .call(
+        d3
+          .axisBottom(xScale)
+          .tickSize(-height + spacing / 2)
+          .tickFormat("")
+          .tickSizeOuter(0)
+          .tickSizeInner(-height + spacing / 2)
+          .tickPadding(10)
+          .tickFormat("")
+      );
+
+    // 2
+    svg
+      .append("g")
+      .attr("class", "grid")
+      .attr("transform", `translate(${spacing / 2}, 0)`)
+      .call(
+        d3
+          .axisLeft(yScale)
+          .tickSize(-width + spacing / 2)
+          .tickFormat("")
+          .tickSizeOuter(0)
+          .tickSizeInner(-width + spacing / 2)
+          .tickPadding(10)
+          .tickFormat("")
+      );
 
     svg
-      .selectAll("rect")
+      .append("g")
+      .attr("id", "xAxis")
+      .attr("transform", `translate(0, ${(height - spacing) / 2})`)
+      .call(xAxis)
+      .selectAll("text")
+      .style("font-size", "14px")
+      .style("font-weight", "bold")
+      .filter(function (d) {
+        return d === 0;
+      })
+      .attr("transform", "translate(-10,0)")
+      .style("text-anchor", "end");
+
+    svg
+      .append("g")
+      .attr("id", "yAxis")
+      .attr("transform", `translate(${(width - spacing) / 2}, 0)`)
+      .call(yAxis)
+      .selectAll("text")
+      .style("font-size", "14px")
+      .style("font-weight", "bold")
+      .attr("transform", "translate(0,-3)")
+      .filter(function (d) {
+        return d === 0;
+      })
+      .remove();
+
+    svg
+      .selectAll("circle")
       .data(data)
       .enter()
-      .append("rect")
-      .attr("x", (d, i) => i * 70)
-      .attr("y", (d, i) => 300 - 10 * d)
-      .attr("width", 65)
-      .attr("height", (d, i) => d * 10)
-      .attr("fill", "green");
-  }
+      .append("circle")
+      .attr("cx", function (d) {
+        return xScale(d.long);
+      })
+      .attr("cy", function (d) {
+        return yScale(d.lat);
+      })
+      .attr("r", 10) // Circle size
+      .style("fill", "blue") // Color of circles
+      .style("opacity", 0.7) // Opacity
+
+      // .on("mouseover", function (d) {
+      //   info.style("display", "block");
+      //   info
+      //     .html(
+      //       `Country: ${d.country}<br>Confirmed Cases: ${d.confirmed}<br>Latitude: ${d.lat}<br>Longitude: ${d.long}`
+      //     )
+      //     .style("left", d3.event.pageX + 10 + "px")
+      //     .style("top", d3.event.pageY - 28 + "px");
+      // })
+      // .on("mouseout", function () {
+      //   info.style("display", "none");
+      // });
+  };
   render() {
-    return <div id={"#" + this.props.id}></div>;
+    return <div>{this.draw()}</div>;
   }
 }
-export default Chart;
--- src/Density/DrawChart.js
+++ src/Density/DrawChart.js
@@ -1,7 +1,157 @@
-import React from 'react'
+let width = 1500;
+let height = 800;
+let spacing = 100;
 
-export default function DrawChart() {
-  return (
-    <div>DrawChart</div>
-  )
+let info = d3
+  .select("#info")
+  .style("position", "absolute") // Positioning
+  .style("background", "white")
+  .style("padding", "10px") // Modify the padding
+  .style("border", "2px solid black") // Modify the border
+  .style("display", "none");
+
+let svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height)
+  .append("g")
+  .attr("transform", `translate(${spacing / 2}, ${spacing / 2})`);
+
+d3.csv(
+  "https://raw.githubusercontent.com/petertran410/data_visual_project/tranngocnhan/src/Density/totalpopulation.csv"
+).then(function (data) {
+  // let date = "3/20/20";
+  let dataByDate = data.map(function (d) {
+    return {
+      CONVINCE: parseFloat(d["CONVINCE"]),
+      ID: parseFloat(d["ID"]),
+      date: parseInt(d["2016"]),
+      // confirmed: parseInt(d[date]),
+    };
+  });
+  console.log(dataByDate);
+  draw(dataByDate);
+});
+function draw(data) {
+  let minCONVINCE = d3.min(data, function (d) {
+    return d.CONVINCE;
+  });
+  let maxCONVINCE = d3.max(data, function (d) {
+    return d.CONVINCE;
+  });
+  let minID = d3.min(data, function (d) {
+    return d.ID;
+  });
+  let maxID = d3.max(data, function (d) {
+    return d.ID;
+  });
+
+  console.log(minLat, maxLat, minLong, maxLong);
+
+  minCONVINCE = Math.ceil(minCONVINCE);
+  maxCONVINCE = Math.ceil(maxCONVINCE);
+  minID = Math.ceil(minID);
+  maxID = Math.ceil(maxID);
+
+  console.log(minCONVINCE, maxCONVINCE, minID, maxID);
+
+  let xScale = d3
+    .scaleLinear()
+    .domain([minID, maxID])
+    .range([0, width - spacing]);
+  let yScale = d3
+    .scaleLinear()
+    .domain([minCONVINCE, maxCONVINCE])
+    .range([height - spacing, 0]);
+
+  var xAxis = d3.axisBottom(xScale);
+  var yAxis = d3.axisLeft(yScale);
+
+  // 1
+  svg
+    .append("g")
+    .attr("class", "grid")
+    .attr("transform", `translate(0, ${height - spacing / 2})`)
+    .call(
+      d3
+        .axisBottom(xScale)
+        .tickSize(-height + spacing / 2)
+        .tickFormat("")
+        .tickSizeOuter(0)
+        .tickSizeInner(-height + spacing / 2)
+        .tickPadding(10)
+        .tickFormat("")
+    );
+
+  // 2
+  svg
+    .append("g")
+    .attr("class", "grid")
+    .attr("transform", `translate(${spacing / 2}, 0)`)
+    .call(
+      d3
+        .axisLeft(yScale)
+        .tickSize(-width + spacing / 2)
+        .tickFormat("")
+        .tickSizeOuter(0)
+        .tickSizeInner(-width + spacing / 2)
+        .tickPadding(10)
+        .tickFormat("")
+    );
+
+  svg
+    .append("g")
+    .attr("id", "xAxis")
+    .attr("transform", `translate(0, ${(height - spacing) / 2})`)
+    .call(xAxis)
+    .selectAll("text")
+    .style("font-size", "14px")
+    .style("font-weight", "bold")
+    .filter(function (d) {
+      return d === 0;
+    })
+    .attr("transform", "translate(-10,0)")
+    .style("text-anchor", "end");
+
+  svg
+    .append("g")
+    .attr("id", "yAxis")
+    .attr("transform", `translate(${(width - spacing) / 2}, 0)`)
+    .call(yAxis)
+    .selectAll("text")
+    .style("font-size", "14px")
+    .style("font-weight", "bold")
+    .attr("transform", "translate(0,-3)")
+    .filter(function (d) {
+      return d === 0;
+    })
+    .remove();
+
+  svg
+    .selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx", function (d) {
+      return xScale(d.long);
+    })
+    .attr("cy", function (d) {
+      return yScale(d.lat);
+    })
+    .attr("r", 10) // Circle size
+    .style("fill", "blue") // Color of circles
+    .style("opacity", 0.7) // Opacity
+    .on("mouseover", function (d) {
+      info.style("display", "block");
+      info
+        .html(
+          `Country: ${d.country}<br>Confirmed Cases: ${d.confirmed}<br>Latitude: ${d.lat}<br>Longitude: ${d.long}`
+        )
+        .style("left", d3.event.pageX + 10 + "px")
+        .style("top", d3.event.pageY - 28 + "px");
+    })
+    .on("mouseout", function () {
+      info.style("display", "none");
+    });
 }

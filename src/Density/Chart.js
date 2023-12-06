3ba1054a00f1249e7a16721247dde4a440aaab7b
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2023-12-06 10:13:38 +0700

    .

--- src/Density/Chart.js
+++ src/Density/Chart.js
@@ -14,14 +14,15 @@ export default class Chart extends Component {
 
   infoChart() {
     d3.csv(
-      "https://raw.githubusercontent.com/petertran410/data_visual_project/tranngocnhan/TongDanSo%20(2).csv"
-    ).then((data) => {
+      "https://raw.githubusercontent.com/petertran410/data_visual_project/tranngocnhan/src/Density/TongDanSo.csv"
+    ).then((data) => { 
+      // let date = "2016";
       let dataByDate = data.map(function (d) {
         return {
-          convince: d(["TÊN TỈNH"]),
-          year: parseInt(d(["NĂM 2016"])),
-          toanQuoc: parseFloat(d(["TOÀN QUỐC"])),
-          // confirmed: parseInt(d[date]),
+          CONVINCE: d["CONVINCE"],
+          ID: d["ID"],
+          year2016: parseFloat(d["2016"]),
+          year2017: parseFloat(d["2017"]),
         };
       });
       console.log(dataByDate);
@@ -41,25 +42,25 @@ export default class Chart extends Component {
       .attr("height", height)
       .append("g")
       .attr("transform", `translate(${spacing / 2}, ${spacing / 2})`);
-    let minCONVINCE = d3.min(data, function (d) {
-      return d.CONVINCE;
+    let minYear2016 = d3.min(data, function (d) {
+      return d.year2016;
     });
-    let maxCONVINCE = d3.max(data, function (d) {
-      return d.CONVINCE;
+    let maxYear2016 = d3.max(data, function (d) {
+      return d.year2016;
     });
     let minID = d3.min(data, function (d) {
-      return d.ID;
+      return d.year2017;
     });
     let maxID = d3.max(data, function (d) {
-      return d.ID;
+      return d.year2017;
     });
 
-    // minCONVINCE = Math.ceil(minCONVINCE);
-    // maxCONVINCE = Math.ceil(maxCONVINCE);
-    // minID = Math.ceil(minID);
-    // maxID = Math.ceil(maxID);
+    minYear2016 = Math.ceil(minYear2016);
+    maxYear2016 = Math.ceil(maxYear2016);
+    minID = Math.ceil(minID);
+    maxID = Math.ceil(maxID);
 
-    console.log(minCONVINCE, maxCONVINCE, minID, maxID);
+    console.log(minYear2016, maxYear2016, minID, maxID);
 
     let xScale = d3
       .scaleLinear()
@@ -67,7 +68,7 @@ export default class Chart extends Component {
       .range([0, width - spacing]);
     let yScale = d3
       .scaleLinear()
-      .domain([minCONVINCE, maxCONVINCE])
+      .domain([minYear2016, maxYear2016])
       .range([height - spacing, 0]);
 
     var xAxis = d3.axisBottom(xScale);
@@ -139,10 +140,10 @@ export default class Chart extends Component {
       .enter()
       .append("circle")
       .attr("cx", function (d) {
-        return xScale(d.long);
+        return xScale(d.year2016);
       })
       .attr("cy", function (d) {
-        return yScale(d.lat);
+        return yScale(d.minYear2016);
       })
       .attr("r", 10) // Circle size
       .style("fill", "blue") // Color of circles

e6dc20f7768024230166d34f50b74c1b4bb091b3
bin <91893958+ITITIU20337@users.noreply.github.com>
2023-12-06 11:52:19 +0700

    change

--- src/Age/draw.js
+++ src/Age/draw.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import * as d3 from "d3";
-const draw = () => {
+const Draw = () => {
   const margin = {top: 60, right: 20, bottom: 50, left: 60};
   const width = 450 - margin.left - margin.right;
   const height = 350 - margin.top - margin.bottom;
@@ -195,4 +195,4 @@ const draw = () => {
  )
 }
 
-export default draw;
+export default Draw;
--- src/Density/Density.js
+++ src/Density/Density.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import draw from './draw'
 
 const Density = () => {
   return (
     <div className='density' id="Density">
-      Density
+      <draw />
     </div>
   )
 }

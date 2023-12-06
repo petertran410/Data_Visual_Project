d316714144ebd652cbfe713945c47c34e1fdd6be
bin <91893958+ITITIU20337@users.noreply.github.com>
2023-12-06 23:01:51 +0700

    asd

--- src/Age/Age.js
+++ src/Age/Age.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import Chart from './Draw';
+// import React from 'react'
+// import Chart from './Draw';
 
-const Age = () => {
-  return (
-    <div className='Age' id="Age">
-        <Chart />
-    </div>
-  )
-}
+// const Age = () => {
+//   return (
+//     <div className='Age' id="Age">
+//         <Chart />
+//     </div>
+//   )
+// }
 
-export default Age;
+// export default Age;
 
 

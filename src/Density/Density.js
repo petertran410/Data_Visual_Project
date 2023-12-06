62f73ebc19615868a32890302ee1524c288bd198
ThienPhuc1802 <91868450+thienphuc1802@users.noreply.github.com>
2023-12-06 11:45:20 +0700

    test

--- src/Density/Density.js
+++ src/Density/Density.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Household from '../Households/Household';
 
 const Density = () => {
   return (
     <div className='density' id="Density">
       Density
+      <Household />
     </div>
   )
 }

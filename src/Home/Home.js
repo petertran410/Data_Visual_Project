ee91cff4f147df716fc3131c4c8f6715b897bd75
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2023-12-24 00:14:27 +0700

    .

---
+++ src/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import VNMap from "./VNMap";
+import Map from "./Map";
+
+export default function Home() {
+  return (
+    <div>
+      <VNMap />
+      {/* <Map /> */}
+    </div>
+  );
+}

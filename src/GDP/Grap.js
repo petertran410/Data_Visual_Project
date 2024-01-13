59576f467951b1eb9d5f5c70494f14ac23af9fd7
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2024-01-13 22:10:14 +0700

    Done

--- src/GDP/Grap.js
+++ src/GDP/Grap.js
@@ -110,7 +110,7 @@ export default class Graph extends Component {
     // set the dimensions and margins of the graph
     var margin = { top: 10, right: 30, bottom: 30, left: 60 },
       // width = 1000 - margin.left - margin.right,
-      width = 600 - margin.left - margin.right,
+      width = 1000 - margin.left - margin.right,
       height = 600 - margin.top - margin.bottom;
 
     // append the svg object to the body of the page
@@ -143,7 +143,7 @@ export default class Graph extends Component {
       .style("text-anchor", "end")
       .attr("font-size", "15px")
       .attr("dx", "1.55em")
-      .attr("dy", ".55em")
+      .attr("dy", ".65em")
     // .attr("transform", "rotate(-90)");
 
     // Add Y axis
